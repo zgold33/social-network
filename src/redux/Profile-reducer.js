c964e423882dc6cdb2b72e9ca9fcd7fcf925697c
zgold33 <zgold33@gmail.com>
2021-05-24 13:46:49 +0300

    41. Reducer

---
+++ src/redux/Profile-reducer.js
@@ -0,0 +1,32 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
+const profileReducer = (state, action) => {
+
+    switch (action.type) {
+        case ADD_POST:
+            let newPost = {id: 5, message: state.newPostText, likesCount: 0};
+            state.posts.push(newPost);
+            state.newPostText = '';
+            return state;
+        case UPDATE_NEW_POST_TEXT:
+            state.newPostText = action.newPostText;
+            return state;
+        default:
+            return state;
+        }
+    }
+
+
+// export const addPostActionCreator = () => {
+//     return {
+//         type: ADD_POST
+//     }
+// }
+
+export const addPostActionCreator = () => ({type: ADD_POST})  // Тоже самое, что и выше
+
+export const updateNewPostTextActionCreator = (textPost) =>
+    ({type: UPDATE_NEW_POST_TEXT, newPostText: textPost})
+
+    export default profileReducer;
